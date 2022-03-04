0d8a8b79548d02252617452764d2f65ecc3e9dc4
rostyk <rostyslav.hereha.ki.2021@lphu.ua>
2022-03-04 22:54:31 +0200

    first deploy

--- main.py
+++ main.py
@@ -1,22 +1,98 @@
 import os
 import telebot
+from telebot import types
 from flask import Flask, request
-
+from parse_news import parse
+from datetime import datetime, timedelta
 
 TOKEN = '5188420032:AAHPoq0xXoR9YJgaSi_Q36-KsToaSwnF1f8'
 APP_URL = f'https://uanews2022.herokuapp.com/{TOKEN}'
 bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
+SEND = False
+LAST_SEND = datetime.time(0, 0, 0)
 
 #bot
 
-@bot.message_handler(commands=['free'])
+@bot.message_handler(commands=['start', 'help'])
 def start(message):
-    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+    SEND = False
+    chat_id = message.chat.id
+    send_message(chat_id, "Hello, Hi there! This bot was created to obtain "
+                          "information about the situation in Ukraine.Send:\n "
+                          "/news - to receive current news in ukraine. \n"
+                          "/stot - to stop notifications. ")
 
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo(message):
-    bot.reply_to(message, message.text)
+@bot.message_handler(commands=['news'])
+def send_news(message):
+    chat_id = message.chat.id
+    if SEND == False:
+        SEND == True
+        news = parse()
+        start_time = datetime.datetime.now() - datetime.timedelta(hours=1)
+        newss = parse()
+        for news in newss:
+            news_time = datetime.datetime.strptime(str(start_time.date()) + ' ' + news['time'], '%Y-%m-%d %H:%M')
+            if news_time > start_time:
+                markup = types.InlineKeyboardMarkup(row_width=1)
+                item = types.InlineKeyboardButton('Перейти', url=news['href'])
+                markup.add(item)
+                if news['subheader'] != None:
+                    title = f" *{news['header']}*\n{news['subheader']}\n*{news['time']}*"
+                else:
+                    title = f" *{news['header']}*\n*{news['time']}*"
+                if news['image'] == None:
+                    LAST_SEND = news_time
+                    bot.send_message(chat_id=chat_id,
+                                    caption=title,
+                                    reply_markup=markup
+                                    )
+                else:
+                    LAST_SEND = news_time
+                    bot.send_photo(chat_id=chat_id,
+                                   parse_mode='Markdown',
+                                   photo=news['image'],
+                                   caption=title,
+                                   reply_markup=markup
+                                   )
+
+        while SEND:
+            now_time = datetime.datetime.now()
+            newss = parse()
+            for news in newss:
+                news_time = datetime.datetime.strptime(str(now_time.date()) + ' ' + news['time'], '%Y-%m-%d %H:%M')
+                if news_time > LAST_SEND:
+                    markup = types.InlineKeyboardMarkup(row_width=1)
+                    item = types.InlineKeyboardButton('Перейти', url=news['href'])
+                    markup.add(item)
+                    if news['subheader'] != None:
+                        title = f" *{news['header']}*\n{news['subheader']}\n*{news['time']}*"
+                    else:
+                        title = f" *{news['header']}*\n*{news['time']}*"
+                    if news['image'] == None:
+                        LAST_SEND = news_time
+                        bot.send_message(chat_id=chat_id,
+                                         caption=title,
+                                         reply_markup=markup
+                                         )
+                    else:
+                        LAST_SEND = news_time
+                        bot.send_photo(chat_id=chat_id,
+                                       parse_mode='Markdown',
+                                       photo=news['image'],
+                                       caption=title,
+                                       reply_markup=markup
+                                       )
+
+
+@bot.message_handler(commands=['stop'])
+def start(message):
+    SEND = False
+
+
+#@bot.message_handler(func=lambda message: True, content_types=['text'])
+#def echo(message):
+#    bot.reply_to(message, message.text)
 
 #server
 
