64641bfa28966bcae93f420e3d5862b57d7b524b
rostyk <rostyslav.hereha.ki.2021@lphu.ua>
2022-03-05 01:03:11 +0200

    first dont working version

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def send_news(message, SEND=SEND):
     if SEND == False:
         print("Im here 1")
         SEND = True
-        start_time = datetime.datetime.now() + datetime.timedelta(hours=1)
+        start_time = datetime.datetime.now() + datetime.timedelta(hours=2)
         start_time = start_time.time()
         print(start_time)
         zero_time = datetime.time(0, 00, 00)
